45414d5f992d68295e1d033f92cfab4c8883cbde
unknown <sergiocampo1989@hotmail.com>
2017-12-05 13:02:35 -0500

    first commit

--- app/src/main/java/com/android/test/rappi/appstorerappi/MainActivity.java
+++ app/src/main/java/com/android/test/rappi/appstorerappi/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.Menu;
